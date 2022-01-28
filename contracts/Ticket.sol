0c61c3899043aab8653f0c551df847702a168625
bekauz <bekauz@protonmail.com>
2022-01-28 20:36:23 +0100

    ticket logic cntd

--- contracts/Ticket.sol
+++ contracts/Ticket.sol
@@ -3,21 +3,37 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 
-contract Ticket {
+contract Ticket is ERC721 {
 
     using Counters for Counters.Counter;
     Counters.Counter private _soldTickets;
 
     address public eventAddress;
 
-    constructor(address _event) {
+    struct TicketInfo {
+        uint256 eventId;
+    }
+
+    mapping(uint256 => address) internal ticketOwners;
+    mapping(uint256 => TicketInfo) internal tickets;
+
+    constructor(address _event)
+        ERC721("NFTicket", "TICKET") {
         eventAddress = _event;
     }
 
-    function buyTicket(uint256 eventId, address buyer) external view returns (uint256) {
+    function buyTicket(uint256 eventId, address buyer) external returns (uint256) {
+        _soldTickets.increment();
+        uint256 ticketId = _soldTickets.current();
         
-        return _soldTickets.current();
+        tickets[ticketId] = TicketInfo(eventId);
+        ticketOwners[ticketId] = buyer;
+
+        _mint(buyer, ticketId);
+
+        return ticketId;
     }
 }
